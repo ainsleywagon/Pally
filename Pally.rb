54c1638f86e0852669a93adbae7b72add8ede21e
Ainsley Wagoner <ainsley.wagoner@gmail.com>
2013-09-25 17:52:34 -0400

    first question of pally

---
+++ Pally.rb
@@ -0,0 +1,7 @@
+print "Name your goal: (i.e. 8 glasses of water a day = 'Water')"
+goal_name = gets.chomp
+goal_name.capitalize!
+
+#often = ["daily", "weekly", "monthly", "yearly"]
+#print "How often do you want to do this?" + often
+#how_often = gets.chomp
