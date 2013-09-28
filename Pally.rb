6974fc6af47a4f883ab2737ec63abbd6b7e88917
Ainsley Wagoner <ainsley.wagoner@gmail.com>
2013-09-27 21:59:22 -0400

    very rough first sequence of question/answer

--- Pally.rb
+++ Pally.rb
@@ -2,6 +2,11 @@ print "Name your goal: (i.e. 8 glasses of water a day = 'Water')"
 goal_name = gets.chomp
 goal_name.capitalize!
 
-#often = ["daily", "weekly", "monthly", "yearly"]
-#print "How often do you want to do this?" + often
-#how_often = gets.chomp
+print "How often do you want to do this? (daily, weekly, monthly, yearly)?" 
+how_often = gets.chomp
+print goal_name + " " + how_often
+
+print "How many times per" + how_often + "?"
+frequency = gets.chomp
+
+print goal_name + " " + frequency + " times " + how_often
